6f8bce3b6087ed2450a7c4a13606a0e3193b2b24
Davide De Rosa <keeshux@gmail.com>
2020-07-07 22:26:54 +0200

    Improve TLS key parsing

--- scripts/net.rb
+++ scripts/net.rb
@@ -12,7 +12,7 @@ servers = File.read("../template/servers.json")
 ca = File.read("../static/ca.crt")
 client = File.read("../static/cert.crt")
 client_key = File.read("../static/cert.key")
-tls_wrap = read_tls_wrap("crypt", 1, "../static/ta.key", 4, 20)
+tls_wrap = read_tls_wrap("crypt", 1, "../static/ta.key", 4)
 
 cfg = {
     ca: ca,
